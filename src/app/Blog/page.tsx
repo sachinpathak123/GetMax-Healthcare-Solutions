3a916e75b117bd01a10747b99476d7027558ad5a
sahil <sahilhussain1231@gmail.com>
2024-01-19 01:16:54 +0530

    started blog page

---
+++ src/app/Blog/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+import './blog.css'
+
+export default function Blog () {
+    return (
+        <>
+            <div >
+                <Image src="/images/blog.jpg" alt="case" className="w-full h-[70vh]" width="1519" height="0"/>
+            </div>
+
+        </>
+    );
+};
